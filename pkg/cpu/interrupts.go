a5f98386b31970d8e26f1fe0dc7a9bc92225d3bb
Tom Johnson <tom@tmjohnson.co.uk>
2019-10-22 19:02:07 +0900

    Tidy up interrupt implementation
    - Name constants
    - Reduce block depth
    - Use clearInterrupt to reduce duplication in serviceInterrupt
    
    NB: code in CheckInterrupts affects performance quite a lot (run in
    main loop). Memory accesses only done once per call to avoid high
    cost.
    
    Should maybe make this a separate package?

--- pkg/cpu/interrupts.go
+++ pkg/cpu/interrupts.go
@@ -4,10 +4,6 @@ import (
 	"github.com/tbtommyb/goboy/pkg/utils"
 )
 
-const InterruptFlagAddress = 0xFF0F
-const InterruptEnableAddress = 0xFFFF
-const InterruptCount = 4
-
 type Interrupt byte
 
 const (
@@ -17,30 +13,46 @@ const (
 	Input                   = 4
 )
 
+const (
+	VBlankInterruptHandlerAddress        uint16 = 0x40
+	LCDCStatusInterruptHandlerAddress           = 0x48
+	TimerOverflowInterruptHandlerAddress        = 0x50
+	InputInterruptHandlerAddress                = 0x60
+)
+
+const (
+	InterruptFlagAddress   uint16 = 0xFF0F
+	InterruptEnableAddress        = 0xFFFF
+)
+
+var Interrupts = []Interrupt{VBlank, LCDCStatus, TimerOverflow, Input}
+
 func (cpu *CPU) requestInterrupt(interrupt Interrupt) {
-	request := cpu.memory.get(InterruptFlagAddress)
-	request = utils.SetBit(byte(interrupt), request, 1)
-	cpu.memory.set(InterruptFlagAddress, request)
+	requested := cpu.memory.get(InterruptFlagAddress)
+	requested = utils.SetBit(byte(interrupt), requested, 1)
+	cpu.memory.set(InterruptFlagAddress, requested)
 }
 
 func (cpu *CPU) clearInterrupt(interrupt Interrupt) {
-	request := cpu.memory.get(InterruptFlagAddress)
-	request = utils.SetBit(byte(interrupt), request, 0)
-	cpu.memory.set(InterruptFlagAddress, request)
+	requested := cpu.memory.get(InterruptFlagAddress)
+	requested = utils.SetBit(byte(interrupt), requested, 0)
+	cpu.memory.set(InterruptFlagAddress, requested)
 }
 
 func (cpu *CPU) CheckInterrupts() {
-	if cpu.interruptsEnabled() {
-		request := cpu.memory.get(InterruptFlagAddress)
-		enabled := cpu.memory.get(InterruptEnableAddress)
-		if request != 0 {
-			for i := byte(0); i <= InterruptCount; i++ {
-				if utils.IsSet(i, request) {
-					if utils.IsSet(i, enabled) {
-						cpu.serviceInterrupt(Interrupt(i))
-					}
-				}
-			}
+	if !cpu.interruptsEnabled() {
+		return
+	}
+
+	requested := cpu.memory.get(InterruptFlagAddress)
+	if requested == 0 {
+		return
+	}
+
+	enabled := cpu.memory.get(InterruptEnableAddress)
+	for i, interrupt := range Interrupts {
+		if utils.IsSet(byte(i), requested) && utils.IsSet(byte(i), enabled) {
+			cpu.serviceInterrupt(interrupt)
 		}
 	}
 }
@@ -48,9 +60,7 @@ func (cpu *CPU) CheckInterrupts() {
 func (cpu *CPU) serviceInterrupt(interrupt Interrupt) {
 	cpu.disableInterrupts()
 
-	request := cpu.memory.get(InterruptFlagAddress)
-	request = utils.SetBit(byte(interrupt), request, 0)
-	cpu.memory.set(InterruptFlagAddress, request)
+	cpu.clearInterrupt(interrupt)
 
 	high, low := utils.SplitPair(cpu.GetPC())
 	cpu.pushStack(high)
@@ -58,12 +68,12 @@ func (cpu *CPU) serviceInterrupt(interrupt Interrupt) {
 
 	switch interrupt {
 	case VBlank:
-		cpu.setPC(0x40)
+		cpu.setPC(VBlankInterruptHandlerAddress)
 	case LCDCStatus:
-		cpu.setPC(0x48)
+		cpu.setPC(LCDCStatusInterruptHandlerAddress)
 	case TimerOverflow:
-		cpu.setPC(0x50)
+		cpu.setPC(TimerOverflowInterruptHandlerAddress)
 	case Input:
-		cpu.setPC(0x60)
+		cpu.setPC(InputInterruptHandlerAddress)
 	}
 }
