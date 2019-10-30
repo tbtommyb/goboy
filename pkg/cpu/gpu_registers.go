4cbca47705b01c7991eb4fd155249c0f10800fd1
Tom Johnson <tom@tmjohnson.co.uk>
2019-10-30 18:03:04 +0900

    Move component-specific registers to component file

--- pkg/cpu/gpu_registers.go
+++ pkg/cpu/gpu_registers.go
@@ -26,14 +26,26 @@ const (
 	MatchInterrupt                       = 6
 )
 
+const LCDCAddress = 0xFF40
+const STATAddress = 0xFF41
+const ScrollYAddress = 0xFF42
+const ScrollXAddress = 0xFF43
+const LYAddress = 0xFF44
+const LYCAddress = 0xFF45
+const BGPAddress = 0xFF47
+const OBP0Address = 0xFF48
+const OBP1Address = 0xFF49
+const WindowYAddress = 0xFF4A
+const WindowXAddress = 0xFF4B
+
 // LCD Control
 
-func (gpu *GPU) getControl() GPUControl {
-	return GPUControl(gpu.cpu.getLCDC())
+func (cpu *CPU) getGPUControl() GPUControl {
+	return GPUControl(cpu.memory.get(LCDCAddress))
 }
 
-func (gpu *GPU) setControl(control GPUControl) {
-	gpu.cpu.setLCDC(byte(control))
+func (cpu *CPU) setGPUControl(control GPUControl) {
+	cpu.memory.set(LCDCAddress, byte(control)|1)
 }
 
 func (control GPUControl) isDisplayEnabled() bool {
@@ -66,12 +78,12 @@ func (control GPUControl) isSet(flag GPUControlFlag) bool {
 
 // LCD Status
 
-func (gpu *GPU) getStatus() GPUStatus {
-	return GPUStatus(gpu.cpu.getSTAT())
+func (cpu *CPU) getGPUStatus() GPUStatus {
+	return GPUStatus(cpu.memory.get(STATAddress))
 }
 
-func (gpu *GPU) setStatus(status GPUStatus) {
-	gpu.cpu.setSTAT(byte(status))
+func (cpu *CPU) setGPUStatus(status GPUStatus) {
+	cpu.memory.set(STATAddress, byte(status))
 }
 
 func (status GPUStatus) mode() Mode {
@@ -100,4 +112,64 @@ func (status GPUStatus) resetMatchFlag() GPUStatus {
 
 func (status GPUStatus) isMatchInterruptSet() bool {
 	return utils.IsSet(byte(MatchInterrupt), byte(status))
+}
+
+func (cpu *CPU) getScrollY() byte {
+	return cpu.memory.get(ScrollYAddress)
+}
+
+func (cpu *CPU) setScrollY(value byte) {
+	cpu.memory.set(ScrollYAddress, value)
+}
+
+func (cpu *CPU) getScrollX() byte {
+	return cpu.memory.get(ScrollXAddress)
+}
+
+func (cpu *CPU) setScrollX(value byte) {
+	cpu.memory.set(ScrollXAddress, value)
+}
+
+func (cpu *CPU) getWindowY() byte {
+	return cpu.memory.get(WindowYAddress)
+}
+
+func (cpu *CPU) getWindowX() byte {
+	return cpu.memory.get(WindowXAddress)
+}
+
+func (cpu *CPU) getLY() byte {
+	return cpu.memory.get(LYAddress)
+}
+
+func (cpu *CPU) setLY(value byte) {
+	cpu.memory.ioram[LYAddress-0xFF00] = value
+}
+
+func (cpu *CPU) getLYC() byte {
+	return cpu.memory.get(LYCAddress)
+}
+
+func (cpu *CPU) getBGP() byte {
+	return cpu.memory.get(BGPAddress)
+}
+
+func (cpu *CPU) setBGP(value byte) {
+	cpu.memory.set(BGPAddress, value)
+}
+
+func (cpu *CPU) setOBP0(value byte) {
+	cpu.memory.set(OBP0Address, value)
+}
+
+func (cpu *CPU) getOBP0() byte {
+	return cpu.memory.get(OBP0Address)
+}
+
+func (cpu *CPU) setOBP1(value byte) {
+	cpu.memory.set(OBP1Address, value)
+}
+
+func (cpu *CPU) getOBP1() byte {
+	return cpu.memory.get(OBP1Address)
 }
